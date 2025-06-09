28b7fc73098800d22fab1a81b7baf125324e2768
luisjro16 <luis_@leptops.>
2025-06-09 16:11:18 -0300

    Integração back-front (#3)

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
-    def create
-        paciente = Paciente.find_by(email: params[:email])
-        if paciente&.authenticate(params[:password])
-          token = JsonWebToken.encode(paciente_id: paciente.id)
-          render json: { token: token }, status: :ok
-        else
-          render json: { error: 'Credenciais inválidas' }, status: :unauthorized
-        end
-    end
+  skip_before_action :authenticate_request, only: [:create]
+
+  def create
+      paciente = Paciente.find_by(username: params[:username])
+      if paciente&.authenticate(params[:password])
+        token = JsonWebToken.encode(paciente_id: paciente.id)
+        render json: { token: token }, status: :ok
+      else
+        render json: { error: 'Credenciais inválidas' }, status: :unauthorized
+      end
+  end
 end
